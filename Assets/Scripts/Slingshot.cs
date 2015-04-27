d67bd7aca2490b0d933198b982ea80d92bd27d0f
Rohan Yang <kim.n.duong@stud.h-da.de>
2015-04-27 11:50:23 +0200

    sphere, last script entry

--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -3,11 +3,20 @@ using System.Collections;
 
 public class Slingshot : MonoBehaviour {
 
+	public GameObject launchPoint;
+
+	void Awake() {
+		Transform launchPointTrans = transform.Find ("LaunchPoint");
+		launchPoint = launchPointTrans.gameObject;
+		launchPoint.SetActive(false);
+	}
+
 	void OnMouseEnter() {
-		print ("Slingshot:OnMouseEnter");
+		//print ("Slingshot:OnMouseEnter");
+		launchPoint.SetActive(false);
 	}
 
 	void OnMouseExit() {
-		print ("Slingshot:OnMouseExit");
+		//print ("Slingshot:OnMouseExit");
 	}
 }
